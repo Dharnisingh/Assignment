808c1c516c5ca367d3e626de44807701d475c0c0
Dharni <dharnisingh@gmail.com>
2020-07-23 00:41:36 +0530

    deployment script

---
+++ deployment_script.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
---
+++ start_api.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# Create the docker image and start the container
+sudo docker-compose -f /home/Docker/docker-compose.yaml up
