cf2ee42d69faf54268f996fc292b10c72efc0bd4
Dharni <dharnisingh@gmail.com>
2020-07-23 00:39:55 +0530

    Updated appspec.yml

--- awsDeploy/deployment_script.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-sudo apt install docker.io -y
-sudo systemctl start docker
-sudo systemctl enable docker
--- awsDeploy/start_api.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# Create the docker image and start the container
-sudo docker-compose -f /home/Docker/docker-compose.yaml up
